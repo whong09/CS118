c7dbf08044fe19c7a7a1a6f11c44a0226c216073
mjsaber <mj.saber1990@gmail.com>
2013-04-24 13:14:59 -0700

    combine

--- http-proxy.cc
+++ http-proxy.cc
@@ -1,5 +1,3 @@
-/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
-
 #include <sstream>
 #include <iostream>
 #include <sys/socket.h>
@@ -14,10 +12,11 @@
 #include <arpa/inet.h> 
 #include "http-request.h"
 #include "http-response.h"
+#include <signal.h>
 
 using namespace std;
 
-string get_remote_page(HttpRequest req)
+HttpResponse get_remote_page(HttpRequest req)
 {
   int sockfd = 0, n = 0, status;
   char recvBuff[1024];
@@ -44,13 +43,22 @@ string get_remote_page(HttpRequest req)
   write(sockfd, sendBuff, strlen(sendBuff));
   stringstream s;
   s.str("");
+  cout << "fuck";
   while((n = read(sockfd, recvBuff, sizeof(recvBuff)-1)) > 0)
   {
-    recvBuff[n] = 0;
-    s << recvBuff;
+	  cout << "am i here?" << endl;
+	  recvBuff[n]=0;
+		s<< recvBuff;
   }
+  cout << s.str() << endl;
   close(sockfd);
-  return s.str();
+  HttpResponse resp;
+  
+  resp.ParseResponse(s.str().c_str(),s.str().length());
+	cout << resp.GetStatusCode() << endl;
+	cout << resp.GetStatusMsg() << endl;
+
+  return resp;
 }
 
 string get_host(HttpRequest * req)
@@ -71,14 +79,11 @@ unsigned short get_port(HttpRequest * req)
 	return req->GetPort();
 }
 
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-int main(int argc, char *argv[])
+HttpRequest client_to_proxy()
 {
-
 	int sockfd, newsockfd, n, pid;
 	int port = 14805;
+	int yes = 1;
 	char buffer[256];
 	struct sockaddr_in server_addr, client_addr;
 	socklen_t client_addr_size;
@@ -88,11 +93,16 @@ int main(int argc, char *argv[])
 	server_addr.sin_family = AF_INET;
 	server_addr.sin_addr.s_addr = INADDR_ANY;
 	server_addr.sin_port = htons(port);
+
+	if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(int)) == -1) {
+	perror("setsockopt"); exit(1);
+	}
+	
 	if (bind(sockfd, (struct sockaddr *) &server_addr,sizeof(server_addr)) < 0)
 		perror("Binding error");
-		
+
 	listen(sockfd,10);
-		
+
 	while (1) 
 	{
 		bzero(&client_addr,sizeof(client_addr));
@@ -119,25 +129,96 @@ int main(int argc, char *argv[])
 				if (n==2 && buffer[0]=='\r' && buffer[1]=='\n')
 					break;
 			}
-			
+
 			HttpRequest client_req;
 			client_req.ParseRequest(client_string.c_str(),client_string.length());
 
 			cout << "The hostname that the client wants is: " << get_host(&client_req) << endl;
 			cout << "Port number of the remote host is: " << get_port(&client_req) << endl;
 			//cout << get_remote_page(client_req);
+			return client_req;
     }
   }
-  
-  /*HttpRequest req;
-  req.SetHost(argv[1]);
+}
+
+int main(int argc, char *argv[])
+{
+	/*
+  int sockfd, newsockfd, n, pid;
+  int port = 14805;
+  char buffer[256];
+  struct sockaddr_in server_addr, client_addr;
+  socklen_t client_addr_size;
+  bzero(&server_addr,sizeof(server_addr));
+  if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    perror("Socket Creating error");
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_addr.s_addr = INADDR_ANY;
+  server_addr.sin_port = htons(port);
+  if (bind(sockfd, (struct sockaddr *) &server_addr,sizeof(server_addr)) < 0)
+    perror("Binding error");
+    
+  listen(sockfd,10);
+    
+  while (1) 
+  {
+    bzero(&client_addr,sizeof(client_addr));
+    newsockfd = accept(sockfd, (struct sockaddr *)&client_addr, &client_addr_size);
+    
+    if (newsockfd < 0)
+      perror("Accepting connection error");
+    
+    pid = fork();
+    
+    if(pid < 0)
+      perror("Forking error");
+    else if (pid == 0)
+    {
+      string client_string="";
+      
+      //getting the request string
+      while(1)
+      {
+        bzero(buffer,256);
+        
+        n = read(newsockfd,buffer,255);
+        
+        if (n < 0)
+          perror("Read request error");
+        
+        client_string.append(buffer,n);
+        
+        //check end with \r\n\r\n
+        //Todo: should use memmem to deal with persistent
+        if (n==2 && buffer[0]=='\r' && buffer[1]=='\n')
+          break;
+      }
+      
+      HttpRequest client_req;
+      
+      client_req.ParseRequest(client_string.c_str(),client_string.length());
+      
+      cout << "The hostname that the client wants is: " << client_req.GetHost() << ":" << client_req.GetPort() << endl;
+      
+      
+      
+
+    }
+  }
+  */
+  HttpRequest req;
+  req=client_to_proxy();
+
+/*  req.SetHost("www.google.com");
   req.SetPort(80);
   req.SetMethod(HttpRequest::GET);
   req.SetPath("/");
   req.SetVersion("1.1");
   //must close connection or hangs
+*/
   req.AddHeader("Connection", "close");
-  string s = get_remote_page(req);
-  cout << s;
-  return 0;*/
+  HttpResponse resp;
+  resp = get_remote_page(req);
+  return 0;
 }
+
