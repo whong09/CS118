710ea4a7fa78d294408197c5de7cf24116ada1d2
Derong Liang <liangderong@yahoo.com>
2013-04-26 19:35:10 -0700

    real persistent and pipelining

--- http-proxy.cc
+++ http-proxy.cc
@@ -13,49 +13,70 @@
 #include "http-request.h"
 #include "http-response.h"
 #include <signal.h>
+#include "compat.h"
 
 using namespace std;
 
+int sendall(int s, char *buf, int *len)
+{
+    int total = 0;        // how many bytes we've sent
+    int bytesleft = *len; // how many we have left to send
+    int n;
+
+    while(total < *len) {
+        n = send(s, buf+total, bytesleft, 0);
+        if (n == -1) { break; }
+        total += n;
+        bytesleft -= n;
+    }
+
+    *len = total; // return number actually sent here
+
+    return n==-1?-1:0; // return -1 on failure, 0 on success
+}
+
 string get_remote_page(HttpRequest req)
 {
-  int sockfd = 0, n = 0, status;
-  char recvBuff[1024];
-  char sendBuff[1024];
-  struct addrinfo *servinfo, *p;
-  struct addrinfo hints;
-  memset(&hints, 0, sizeof(hints));
-  hints.ai_family = PF_INET;
-  hints.ai_flags = AI_PASSIVE;
-  hints.ai_socktype = SOCK_STREAM;
-  memset(recvBuff, 0,sizeof(recvBuff));
-  memset(sendBuff, 0, sizeof(sendBuff));
-  if((status = getaddrinfo(req.GetHost().c_str(), "http", &hints, &servinfo)) != 0)
-    perror(gai_strerror(status));
-  for(p = servinfo; p != NULL; p = p->ai_next) {
-	cout << "checking ip" << endl;
-    if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
-      continue;
-    }
-    if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
-      close(sockfd);
-      continue;
-    }
-    break;
-  }
-cout << "socket created" << endl;
-  if(p==NULL)
-	perror("Connect Error");
-  freeaddrinfo(servinfo);
-  req.FormatRequest(sendBuff);
-  write(sockfd, sendBuff, req.HttpRequest::GetTotalLength());
-  string s="";
-  while((n = read(sockfd, recvBuff, sizeof(recvBuff)-1)) > 0)
-  {
-	cout << "am i here?" << endl;
-	s.append(recvBuff,n);
-  }
-  close(sockfd);
-  return s;
+	int sockfd = 0, n = 0, status;
+	char recvBuff[100000];
+	char sendBuff[100000];
+	struct addrinfo *servinfo, *p;
+	struct addrinfo hints;
+	string s="";
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = PF_INET;
+	hints.ai_flags = AI_PASSIVE;
+	hints.ai_socktype = SOCK_STREAM;
+	memset(recvBuff, 0, 100000);
+	memset(sendBuff, 0, 100000);
+	if((status = getaddrinfo(req.GetHost().c_str(), "http", &hints, &servinfo)) != 0)
+		perror(gai_strerror(status));
+	for(p = servinfo; p != NULL; p = p->ai_next) {
+		if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1) {
+	    	continue;
+		}
+		if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+			close(sockfd);
+			continue;
+		}
+		break;
+	}
+	if(p==NULL)
+		perror("Connect Error");
+	freeaddrinfo(servinfo);
+	req.FormatRequest(sendBuff);
+	int length = req.HttpRequest::GetTotalLength();
+	if(sendall(sockfd,sendBuff,&length) == -1)
+	{
+		perror("Sending request error");
+	}
+	while((n = recv(sockfd, recvBuff, sizeof(recvBuff)-1,0)) > 0)
+	{
+		//cout << "am i here?" << endl;
+		s.append(recvBuff,n);
+	}
+	close(sockfd);
+	return s;
 }
 
 string get_host(HttpRequest * req)
@@ -82,7 +103,6 @@ int main(int argc, char *argv[])
 {
 	int sockfd, newsockfd, n, pid;
 	int port = 14805;
-	char buffer[256];
 	struct sockaddr_in server_addr, client_addr;
 	socklen_t client_addr_size;
 	bzero(&server_addr,sizeof(server_addr));
@@ -110,6 +130,8 @@ int main(int argc, char *argv[])
 	    	perror("Forking error");
 	    else if (pid == 0)
 	    {
+			cout << "new process #" << getpid() << endl;
+			char buffer[256];
 			string from_client_str;
 			while(1)
 			{
@@ -123,35 +145,30 @@ int main(int argc, char *argv[])
 		        		perror("Read request error");
 		        	from_client_str.append(buffer,n);
 		        	//check end with \r\n\r\n
-		        	//Todo: should use memmem to deal with persistent
-		        	if (n==2)
+		        	if (memmem(from_client_str.c_str(), from_client_str.length(), "\r\n\r\n", 4) != NULL)
 		        		break;
 				}
-		      	HttpRequest client_req;			
+		      	HttpRequest client_req;
 		      	client_req.ParseRequest(from_client_str.c_str(),from_client_str.length());
-		      	cout << "The hostname that the client wants is: " << get_host(&client_req) <<  endl;
-				cout << "The port number is: " << get_port(&client_req) << endl;
-		      	client_req.AddHeader("Connection", "close");
+				get_host(&client_req);
+				get_port(&client_req);
+				client_req.ModifyHeader("Connection","close");
 			  	string s = get_remote_page(client_req);
-			  	cout << s << endl;
 				HttpResponse client_resp;
 				client_resp.ParseResponse(s.c_str(),s.length());
-
-				write(newsockfd,s.c_str(),s.length());
-				cout << client_resp.FindHeader("Date") << endl;
+				int old_len = client_resp.HttpResponse::GetTotalLength();
+				client_resp.ModifyHeader("Connection","keep-alive");
+				int len = client_resp.HttpResponse::GetTotalLength();
+				char sendBuff[len+1];
+				client_resp.FormatResponse(sendBuff);
+				string sendstring = "";
+				sendstring.append(sendBuff,len);
+				sendstring.append(s.substr(old_len).c_str(),s.length()-old_len);
+				if(write(newsockfd,sendstring.c_str(),sendstring.length()) == -1)
+					perror("Sending response error");
 			}
 	    }
 	}
-
-	  //HttpRequest req;
-	  //req=client_to_proxy();
-	/*req.SetHost("www.google.com");
-	  req.SetPort(80);
-	  req.SetMethod(HttpRequest::GET);
-	  req.SetPath("/");
-	  req.SetVersion("1.1");
-	  //must close connection or hangs
-	*/
 	  return 0;
 }
 
